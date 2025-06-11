59ef7136b923f2efea43d77ab6edf8d654e36edd
Vaporise <draxdragonfire@googlemail.com>
2025-06-11 09:38:42 +0900

    Added comments for how my code works

--- main.py
+++ main.py
@@ -11,25 +11,25 @@ client = genai.Client(api_key=api_key)
 from google.genai import types
 
 
-args = sys.argv[1:]
+args = sys.argv[1:] #Takes all the arguments after the script name.
 
-prompt_info = [arg for arg in args if arg != "--verbose"]
+prompt_info = [arg for arg in args if arg != "--verbose"] # Takes all the input except for --verbose
 
-user_prompt = " ".join(prompt_info)
+user_prompt = " ".join(prompt_info) #rejoins the strings together.
 
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
-]
+] 
 
-query = client.models.generate_content(model= "gemini-2.0-flash-001", contents=messages)
+query = client.models.generate_content(model= "gemini-2.0-flash-001", contents=messages) #calls the gemini api using the user_input
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 1: #Checks that input is more than one to confirm that an input has been used otherwise closes the program
 
-    if user_prompt == "":
+    if user_prompt == "": #If not input is entered
         print("No prompt was entered")
         sys.exit(1)
     
-    if "--verbose" in sys.argv:
+    if "--verbose" in sys.argv: #If verbose is used as an argument
         print(f"User prompt: {user_prompt}")
         try:
             print(f"Prompt tokens: {query.usage_metadata.prompt_token_count}")
@@ -40,7 +40,7 @@ if len(sys.argv) > 1:
         except AttributeError as t:
             print(t)
     
-    print(query.text)  
+    print(query.text)  #if verbose is not used and prompt is not empty it directly prints the LLM response
 
     
 else:
