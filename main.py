0280509cdbbbf677ef6db77b0a8bf88680dd9b41
Vaporise <draxdragonfire@googlemail.com>
2025-06-11 09:18:01 +0900

    Added in a promt that takes --verbose and outputs extra information

--- main.py
+++ main.py
@@ -10,24 +10,35 @@ client = genai.Client(api_key=api_key)
 
 from google.genai import types
 
+
+args = sys.argv[1:]
+
+prompt_info = [arg for arg in args if arg != "--verbose"]
+
+user_prompt = " ".join(prompt_info)
+
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
 ]
 
-
+query = client.models.generate_content(model= "gemini-2.0-flash-001", contents=messages)
 
 if len(sys.argv) > 1:
-    query = client.models.generate_content(model= "gemini-2.0-flash-001", contents=messages)
-    print(query.text)
+    
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        try:
+            print(f"Prompt tokens: {query.usage_metadata.prompt_token_count}")
+        except AttributeError as e:
+            print(e)
+        try:
+            print(f"Response tokens: {query.usage_metadata.candidates_token_count}")
+        except AttributeError as t:
+            print(t)
+    
+    print(query.text)  
 
-    try:
-        print(f"Prompt tokens: {query.usage_metadata.prompt_token_count}")
-    except AttributeError as e:
-        print(e)
-    try:
-        print(f"Response tokens: {query.usage_metadata.candidates_token_count}")
-    except AttributeError as t:
-        print(t)
+    
 else:
     print("No arguments were entered.")
     sys.exit(1)
