47ac41db0f2d93a374153f589e8b9b1ba2f6b2b8
Vaporise <draxdragonfire@googlemail.com>
2025-08-27 14:38:25 +0900

    Added code to create the outputted response at the end, still need to correct it

--- functions/run_python.py
+++ functions/run_python.py
@@ -7,16 +7,44 @@ def run_python_file(working_directory, file_path, args=[]):
 
     absolute_filepath= os.path.abspath(mainpath)
     absolute_working_directory = os.path.abspath(working_directory)
+    try:
 
-    if not absolute_filepath.startswith(absolute_working_directory):
+                if not absolute_filepath.startswith(absolute_working_directory):
 
-            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
-    
-    elif os.path.isfile(mainpath) == False:
-            return f'Error: File "{file_path}" not found.'
-    
-    elif not file_path.endswith('.py'):
-            return f'Error: "{file_path}" is not a Python file.'
+                        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
+                elif os.path.isfile(mainpath) == False:
+                        return f'Error: File "{file_path}" not found.'
+        
+                elif not file_path.endswith('.py'):
+                        return f'Error: "{file_path}" is not a Python file.'
 
-    else:
-           completed_process = subprocess.run(args, capture_output = True, timeout = 30,)
+                else:
+                        completed_process = subprocess.run(['python3',file_path,*args], cwd=working_directory, capture_output = True, timeout = 30)
+
+                        decoded_stdout = completed_process.stdout.decode('utf-8')
+                        decoded_stderr = completed_process.stderr.decode('utf-8')
+
+                        final_output = []
+
+                        if decoded_stdout:
+                                final_output.append(decoded_stdout)
+
+                        if decoded_stderr:
+                                final_output.append(decoded_stderr)
+
+                        if decoded_stdout and decoded_stderr:
+                                
+
+                        if completed_process.returncode and not 0:
+                                final_output += f" Process exited with code {completed_process.returncode}"
+                                return final_output
+                        
+                        if completed_process.stdout == b'' and completed_process.stderr == b'' and completed_process.returncode == 0:
+                                return "No output produced."
+ 
+                        else:
+                                return "\n".join(final_output)
+                
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
